36ba78162672558defc16d3ea56f664b69f7f177
Rex-tr909 <62932902+Rex-tr909@users.noreply.github.com>
2023-02-12 16:16:38 +1100

    Navigation Fixes
    - Fixes to navigation
    - Styling updates
    - Crowd simulation description added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const project1 = document.getElementById("pc0");
 const project1Page = document.getElementById("p1Page");
+const homeButton = document.getElementById("homeButton");
 const allProjects = document.getElementsByClassName("projectContainer");
+let currentlyDisplayed = "";
 
-if (project1 != null) {
+window.onload = () => {
+    homeButton.addEventListener('click', () => Navigate("?"))
     project1.addEventListener('click', () => Navigate("?project=1"));
 }
 
 function Navigate(location) {
+    if (location == "?") {
+        HideProject();
+        window.history.replaceState({ page: 1 }, "title 0", location);
+        return;
+    }
+
     HideProjectsLinks();
     window.history.replaceState({ page: 2 }, "title 2", location);
 }
@@ -16,17 +25,39 @@ function HideProjectsLinks() {
         projectDiv.classList.remove('fade-in-bottom');
         projectDiv.classList.add('fade-out-bottom');
         let currentDiv = projectDiv;
-        setTimeout(() => FinishHiding(currentDiv), 1000);
+        setTimeout(() => FinishHiding(currentDiv, project1Page), 1000);
     }
 }
 
-function FinishHiding(projectDiv) {
-    projectDiv.style.display = 'none';
-    projectDiv.classList.remove('fade-out-bottom');
-    projectDiv.classList.add('fade-in-bottom');
-    ShowProject();
+function FinishHiding(div, newLocation) {
+    div.style.display = 'none';
+    div.classList.remove('fade-out-bottom');
+    div.classList.add('fade-in-bottom');
+    ShowTargetPage(newLocation);
 }
 
-function ShowProject() {
-    project1Page.style.display = 'block';
+function ShowTargetPage(target) {
+    if (target == "") {
+        ShowProjectsLinks();
+    }
+    else ShowProject(target);
+}
+
+function ShowProjectsLinks() {
+    for (projectDiv of allProjects) {
+        projectDiv.style.display = 'flex';
+        projectDiv.classList.remove('fade-out-bottom');
+        projectDiv.classList.add('fade-in-bottom');
+    }
+}
+
+function ShowProject(project) {
+    project.style.display = 'block';
+    currentlyDisplayed = project;
+}
+
+function HideProject() {
+    currentlyDisplayed.classList.remove('fade-in-bottom');
+    currentlyDisplayed.classList.add('fade-out-bottom');
+    setTimeout(() => FinishHiding(currentlyDisplayed, ""), 1000);
 }
