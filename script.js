64a2d78fae6510e8ac8a8986e95ac249ff5f9468
Rex-tr909 <62932902+Rex-tr909@users.noreply.github.com>
2025-04-27 11:10:46 +1000

    - WebGL shader background - Sideways name

--- script.js
+++ script.js
@@ -1,75 +1,136 @@
 const homeButton = document.getElementById("homeButton");
 const returnHomeButton = document.getElementById("returnHomeButton");
-const contactButton = document.getElementById("contactButton");
 const homePage = document.getElementById("homePage");
 const contactPage = document.getElementById("contactPage");
+const contactMeText = document.getElementById("contactMeText");
 const bodyElement = document.getElementById("body");
+
+shaderBG(bodyElement);
 
 let currentlyDisplayed = "";
 
 if (window.location.search == "?contact")
-    FinishHiding(homePage, ShowTargetPage(contactPage));
+    finishHiding(homePage, showTargetPage(contactPage));
 
 window.onload = () => {
     bodyElement.classList.remove('hidden');
-    homeButton.addEventListener('click', () => Navigate());
-    contactButton.addEventListener('click', () => Navigate("?contact"));
+    homeButton.addEventListener('click', () => navigate());
 }
 
-function Navigate(location) {
+function navigate(location) {
     if (location == null) {
         if (window.location.search == "?contact") {
-            HideContactPage();
+            hideContactPage();
             window.history.replaceState({ page: 1 }, "Home", "?");
             return;
         }
         else {
-            HideHomePage(contactPage);
+            hideHomePage(contactPage);
             window.history.replaceState({ page: 2 }, "Contact", "?contact");
             return;
         }
     }
 
     if (location == "?") {
-        HideContactPage();
+        hideContactPage();
         window.history.replaceState({ page: 1 }, "Home", location);
         return;
     }
 
-    HideHomePage(contactPage);
+    hideHomePage(contactPage);
     window.history.replaceState({ page: 2 }, "Contact", location);
 }
 
-function HideHomePage(newLocation) {
+function hideHomePage(newLocation) {
     homePage.classList.add('fade-out-bottom');
-    setTimeout(() => FinishHiding(homePage, ShowTargetPage(newLocation)), 1000);
+    setTimeout(() => finishHiding(homePage, showTargetPage(newLocation)), 1000);
 }
 
-function FinishHiding(div, callback) {
+function finishHiding(div, callback) {
     div.style.display = 'none';
     div.classList.remove('fade-out-bottom');
     () => callback();
 }
 
-function ShowTargetPage(target) {
+function showTargetPage(target) {
     if (target == "") {
-        ShowHomePage();
+        showHomePage();
     }
-    else ShowPage(target);
+    else showPage(target);
 }
 
-function ShowHomePage() {
+function showHomePage() {
     homePage.style.display = 'flex';
     homePage.classList.remove('fade-out-bottom');
+    contactMeText.textContent = "Contact Me";
 }
 
-function ShowPage(page) {
+function showPage(page) {
     page.style.display = 'flex';
     currentlyDisplayed = page;
+    contactMeText.textContent = "Go Back";
 }
 
-function HideContactPage() {
+function hideContactPage() {
     currentlyDisplayed.classList.remove('fade-in-bottom');
     currentlyDisplayed.classList.add('fade-out-bottom');
-    setTimeout(() => FinishHiding(currentlyDisplayed, ShowTargetPage("")), 1000);
+    setTimeout(() => finishHiding(currentlyDisplayed, showTargetPage("")), 1000);
+}
+
+async function shaderBG(appContainer) {
+    const canvasContainer = document.createElement("div");
+    canvasContainer.style.height = "100%";
+    canvasContainer.style.width = "100%";
+    canvasContainer.style.overflow = "hidden";
+    canvasContainer.style.position = "fixed";
+    canvasContainer.style.top = "0";
+    canvasContainer.style.pointerEvents = "auto";
+    canvasContainer.style.zIndex = -1;
+    canvasContainer.style.filter = "hue-rotate(-45deg) saturate(0.5)";
+    appContainer.append(canvasContainer);
+
+    const appCanvas = document.createElement("canvas");
+    appCanvas.style.border = "1px dotted black";
+    appCanvas.height = canvasContainer.clientHeight;
+    appCanvas.width = canvasContainer.clientWidth;
+    appCanvas.style.height = "100%";
+    appCanvas.style.width = "100%";
+    canvasContainer.append(appCanvas);
+
+    window.onresize = () => {
+        appCanvas.height = canvasContainer.clientHeight;
+        appCanvas.width = canvasContainer.clientWidth;
+    }
+
+    const gl = appCanvas.getContext("webgl2");
+    let mousePosition = { x: 0, y: 0 };
+    appCanvas.onmousemove = e => { mousePosition.x = e.clientX; mousePosition.y = e.clientY };
+
+    const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+
+    const arrays = {
+        position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+    };
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+    function render(time) {
+        twgl.resizeCanvasToDisplaySize(gl.canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        const uniforms = {
+            time: time * 0.001,
+            resolution: [gl.canvas.width, gl.canvas.height],
+            mouse: [mousePosition.x, mousePosition.y], 
+            random: Math.random()
+        };
+
+        gl.useProgram(programInfo.program);
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+        twgl.setUniforms(programInfo, uniforms);
+        twgl.drawBufferInfo(gl, bufferInfo);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
